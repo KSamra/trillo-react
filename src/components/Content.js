8bc2dc0b7034570ed3864f25ec95cfe768c5e9b3
Kavan Samra <kssamra@ucsc.edu>
2020-02-24 21:34:21 -0800

    just changed background color of the hotel view

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Content = styled.div`
   display: flex;
-  height: 80rem;
+
 
 `;
 
--- src/components/HotelView/HotelView.js
+++ src/components/HotelView/HotelView.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 
 const HotelView = styled.main`
   background-color: #fff;
-  background-color: orangered;
   flex: 1;
+
+  height: 80rem;
 
 `;
 export default HotelView;
